5e18742ad37beb55d2893241204cc6a9ca93f591
jananpatel2002 <jkpatel2001@student.stcc.edu>
2022-11-23 18:40:25 -0500

    Finished product, changed scoreboard.py to not show turtle

--- scoreboard.py
+++ scoreboard.py
@@ -7,9 +7,12 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.color("black")
+        self.penup()
+        self.ht()
         self.goto(-280, 260)
         self.level = 0
         self.update_scoreboard()
+
 
     def update_scoreboard(self):
         self.clear()
